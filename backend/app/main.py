fab053820d3303b3277d32a5cc35ddea1173dab2
LucianoLosardo <luloroca@yahoo.com>
2026-03-02 17:33:04 -0300

    configuracion inicial y orden de carpetas

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+#from fastapi import FastAPI
+
+#app = FastAPI(title="SafeGuard API")
+
+#@app.get("/")
+#def read_root():
+ #   return {
+  #      "status": "online",
+   #     "message": "Sistema de Gestión de Matafuegos v1.0",
+    #    "engineer": "Luciano"
+    #}
+
+print("Hello World")
